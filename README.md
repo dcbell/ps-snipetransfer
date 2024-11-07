e543b6b5f0fcafc447c868511ddf17634ba4cf44
dcbell <davidbell.personal@gmail.com>
2024-11-07 02:34:40 -0600

    initial upload

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # ps-snipetransfer
-Powershell functions to transfer assets and accessories in Snipe-IT.
+
+Powershell function to transfer assets from one user to another.
+
+> [!IMPORTANT]
+> References to IDs (asset IDs, User IDs, etc.) are references to the database IDs. You can find these in the URL when looking at a user, asset, etc. This is **NOT** the asset tag or the username.
+
+> [!CAUTION]
+> If you are using a CSV or other bulk method for processing transfers, you may want to add a sleep timer between users you are processing. Lots of transfers in a short time will exceed the API rate limit and you will start seeing a lot of errors. Alternatively, if you are hosting your own Snipe-IT server, you can change the API rate limit.
+
+## Future Updates
+
+* Build the necessary files to make the functions here into cmdlets.
+* Add additional functions as needed (display list of assets for a user, etc.)
+
+## Quickstart
+
+You can either paste the entire code into a PowerShell terminal and then execute functions as needed, or build out a main script below the functions.
+
+## Examples
+
+### Transfer everything
+
+To transfer everything from one user or another:
+
+```powershell
+New-SnipeTransferAll -FromID <OldUserID> -ToID <NewUserID>
+```
+
+### Transfer one asset from a user to another user
+
+```powershell
+New-SnipeTransfer -TransferID <Asset ID> -ToID <User ID>
+```
+
+### Transfer one accessory from a user to another user
+
+```powershell
+New-SnipeTransfer -TransferType 'accessory' -TransferID <accessory+user Pivot ID> -AccessoryID <Accessory ID> -ToID <User ID>
+```
+
+### Transfer one asset from a user to an asset
+
+```powershell
+New-SnipeTransfer -TransferID <Asset ID> -ToType 'asset' -ToID <Asset ID>
---
+++ Snipeit-Transfer.ps1
@@ -0,0 +1,249 @@
+### Connection info. Set URL and API personal access token.
+$SnipeURL = Get-Content -Path './secure/url.txt' -Raw
+$ApiKey = Get-Content -Path './secure/key.txt' -Raw
+
+function New-SnipeCheckin {
+	<#
+		.SYNOPSIS
+		Check in an asset or accessory.
+
+		.DESCRIPTION
+		Check in an asset or accessory.
+
+		.PARAMETER CheckinType
+		[string] Optional. Defaults to 'asset'. Type of object (asset or accessory) to check in.
+
+		.PARAMETER CheckinID
+		[int] Required. ID of the asset or accessory+user (not accessory ID) to check in.
+	#>
+
+	param (
+		[Parameter(Mandatory=$false)]
+		[string]$CheckinType="asset",
+
+        [Parameter(Mandatory=$true)]
+        [int]$CheckinID
+	)
+
+	# Set API headers
+	$headers=@{}
+	$headers.Add("accept", "application/json")
+	$headers.Add("Authorization", "Bearer $ApiKey")
+
+	switch ($CheckinType) {
+		"asset" { 
+			$headers.Add("content-type", "application/json")
+			$ApiUrl = "$SnipeURL/api/v1/hardware/$CheckinID/checkin" 
+			$response = Invoke-WebRequest -Uri $ApiUrl -Method POST -Headers $headers -ContentType 'application/json' -Body "{ `"status_id`":null}" | ConvertFrom-Json
+		}
+		"accessory" { 
+			$ApiUrl = "$SnipeURL/api/v1/accessories/$CheckinID/checkin"
+			$response = Invoke-WebRequest -Uri $ApiUrl -Method POST -Headers $headers | ConvertFrom-Json
+		}
+		Default { Throw "Invalid object type. Value must be 'asset' or 'accessory'. Value provided was $CheckinType." }
+	}
+
+	if ($response.status -eq "error") { Throw "Check in failed. Error: $($response.messages)" }
+}
+
+function New-SnipeCheckout {
+	<#
+		.SYNOPSIS
+		Check out an asset or accessory.
+
+		.DESCRIPTION
+		Check out an asset or accessory.
+
+		.PARAMETER CheckoutType
+		[string] Optional. Defaults to 'asset'. Type of object (asset or accessory) to check out.
+
+		.PARAMETER CheckoutID
+		[int] Required. ID of the asset or accessory+user (not accessory ID) to check out.
+
+		.PARAMETER CheckoutToType
+		[string] Optional. Defaults to 'user'. Type of object (user, location, or asset) to check out the object to.
+
+		.PARAMETER CheckoutToID
+		[int] Required. ID to check out the object to.
+	#>
+
+	param (
+		[Parameter(Mandatory=$false)]
+		[string]$CheckoutType="asset",
+
+        [Parameter(Mandatory=$true)]
+        [int]$CheckoutID,
+
+		[Parameter(Mandatory=$false)]
+		[string]$CheckoutToType="user",
+
+		[Parameter(Mandatory=$true)]
+		[int]$CheckoutToID
+	)
+
+	# Check the CheckoutToType
+	if ($CheckoutType -eq "asset" -and $CheckoutToType -notin @("user", "location", "asset")) { throw "Invalid type to check out to. Value must be 'user', 'location', or 'asset'." }
+	if ($CheckoutType -eq "accessory" -and $CheckoutToType -notin @("user")) { throw "Invalid type to check out to. Accessories can only be checked out to 'user'. This is a Snipe-IT limitation." }
+
+	# Set API headers
+	$headers=@{}
+	$headers.Add("accept", "application/json")
+	$headers.Add("Authorization", "Bearer $ApiKey")
+	$headers.Add("content-type", "application/json")
+
+	switch ($CheckoutType) {
+		"asset" { 
+			$ApiUrl = "$SnipeURL/api/v1/hardware/$CheckoutID/checkout" 
+			$response = Invoke-WebRequest -Uri $ApiUrl -Method POST -Headers $headers -ContentType 'application/json' -Body "{ `"checkout_to_type`":`"$CheckoutToType`", `"assigned_$CheckoutToType`":$CheckoutToID }" | ConvertFrom-Json
+		}
+		"accessory" { 
+			$ApiUrl = "$SnipeURL/api/v1/accessories/$CheckoutID/checkout"
+			$response = Invoke-WebRequest -Uri $ApiUrl -Method POST -Headers $headers -ContentType 'application/json' -Body "{ `"checkout_qty`":1,`"assigned_$CheckoutToType`":$CheckoutToID }" | ConvertFrom-Json
+		}
+		Default { Throw "Invalid object type. Value must be 'asset' or 'accessory'. Value provided was $CheckoutType." }
+	}
+
+	if ($response.status -eq "error") { Throw "Check out failed. Error: $($response.messages)" }
+}
+
+function New-SnipeTransfer {
+	<#
+		.SYNOPSIS
+		Transfer an asset or accessory from one user, location, or asset to another.
+
+		.DESCRIPTION
+		Transfer an asset or accessory from one user, location, or asset to another.
+
+		.PARAMETER ToType
+		[string] Optional. Defaults to 'user'. Specifies what to transfer to (user, location, or asset). If transferring an accessory, must be a user.
+
+		.PARAMETER ToID
+		[int] Required. ID of the object to transfer to.
+
+		.PARAMETER TransferType
+		[string] Optional. Defaults to 'asset'. Specifies what to transfer (asset or accessory).
+
+		.PARAMETER TransferID
+		[int] Required. ID of the object to transfer. If transferring an accessory, this will be the accessory+user pivot id.
+
+		.PARAMETER AccessoryID
+		[int] Optional. Only used if transferring an accessory since checkin uses the accessory+user pivot id (TransferID) and the checkout uses the accessory ID.
+	#>
+
+	param (
+		[Parameter(Mandatory=$false)]
+        [string]$ToType='user',
+
+        [Parameter(Mandatory=$true)]
+        [int]$ToID,
+
+		[Parameter(Mandatory=$false)]
+        [string]$TransferType='asset',
+
+        [Parameter(Mandatory=$true)]
+        [int]$TransferID,
+
+		[Parameter(Mandatory=$false)]
+		[int]$AccessoryID
+    )
+
+	# Check in the object
+	Write-Host "Checking in $TransferType $TransferID"
+	New-SnipeCheckin -CheckinType $TransferType -CheckinID $TransferID
+
+	# Check out the object
+	switch ($TransferType) {
+		"asset" { 
+			Write-Host "Checking out $TransferType $TransferID to $ToType $ToID"
+			New-SnipeCheckout -CheckoutType $TransferType -CheckoutID $TransferID -CheckoutToType $ToType -CheckoutToID $ToID 
+		}
+		"accessory" { 
+			Write-Host "Checking out $TransferType $AccessoryID to $ToType $ToID"
+			New-SnipeCheckout -CheckoutType $TransferType -CheckoutID $AccessoryID -CheckoutToType $ToType -CheckoutToID $ToID 
+		}
+		Default { Throw "Error checking out object. Invalid object type. Value must be 'asset' or 'accessory'. Value provided was $CheckoutType." }
+	}	
+}
+
+function New-SnipeTransferAll {
+	<#
+		.SYNOPSIS
+		Transfer all assets and accessories from one user to another.
+
+		.DESCRIPTION
+		Transfer all assets and accessories from one user to another. The API doesn't support pulling a list of everything checked out to an asset or location, but transferring everything from an asset or location could be added here if those functions are added to the API.
+
+		.PARAMETER FromID
+		[int] Required. ID of the object to transfer from.
+
+		.PARAMETER ToID
+		[int] Required. ID of the object to transfer to.
+	#>
+
+	param (
+        [Parameter(Mandatory=$true)]
+        [int]$FromID,
+
+		[Parameter(Mandatory=$true)]
+        [int]$ToID
+    )
+
+	#Forcing FromType and ToType If they add pulling all checked out assets/accessories to locations and assets, this will become a parameter.
+	$FromType="user"
+	$ToType = "user"
+
+	# Set API headers
+	$headers=@{}
+	$headers.Add("accept", "application/json")
+	$headers.Add("Authorization", "Bearer $ApiKey")
+
+	$AssetsToTransfer=@()
+	$AccessoryIDs=@()
+
+	switch ($FromType) {
+		"user" {
+			Write-Host "Fetching assets and accessories assigned to $FromType $FromID"
+			$ApiUrl = "$SnipeURL/api/v1/users/$FromID/assets"
+			$AssetsToTransfer = (Invoke-WebRequest -Uri $ApiUrl -Method GET -Headers $headers  | ConvertFrom-Json).rows
+			$ApiUrl = "$SnipeURL/api/v1/users/$FromID/accessories"
+			$AccessoryIDs = (Invoke-WebRequest -Uri $ApiUrl -Method GET -Headers $headers  | ConvertFrom-Json).rows
+			$AccessoryIDs = $AccessoryIDs | Sort-Object id | Select-Object -Unique id
+		 }
+		Default { Throw "Error starting transfer. Invalid object type. Value must be 'user', 'location', or 'asset'. Value provided was $CheckoutType." }
+	}
+
+	# Iterate through the Accessory IDs and find the accessory+user pivot IDs to be able to check them in.
+	$AccessoriesToTransfer = @()
+	$AccessoryIDs | foreach-object {
+		$TempAssetID = $_.id
+		# get a list of all checked out instances of this accessory
+		$ApiUrl = "$SnipeURL/api/v1/accessories/$($_.id)/checkedout"
+		$response = (Invoke-WebRequest -Uri $ApiUrl -Method GET -Headers $headers  | ConvertFrom-Json).rows
+		# check each checked out instance to see if the user matches the FromID
+		$response | foreach-object {
+			if($($_.assigned_to.id) -eq $FromID) { 
+				$row = [PSCustomObject]@{
+					PivotID = $_.id
+					AssetID = $TempAssetID
+				}
+				$AccessoriesToTransfer += $row 
+			}
+		}
+	}
+
+	# If the From object had any assets, transfer them.
+	if ($AssetsToTransfer.Count -gt 0){
+		$AssetsToTransfer | foreach-object { 
+			Write-Host "Transferring asset $($_.id) from $FromType $FromID to $ToType $ToID"
+			New-SnipeTransfer -ToID $ToID -TransferID $_.id 
+		}
+	} else { Write-Host "No assets assigned to $FromType $FromID" }
+
+	# If the From object had any accessories, transfer them.
+	if ($AccessoriesToTransfer.Count -gt 0){
+		$AccessoriesToTransfer | foreach-object { 
+			Write-Host "Tranferring accessory $_ from $FromType $FromID to $ToType $ToID"
+			New-SnipeTransfer -TransferType 'accessory' -ToID $ToID -TransferID $_.PivotID -AccessoryID $_.AssetID
+		}
+	} else { Write-Host "No accessories assigned to $FromType $FromID" }
+}
