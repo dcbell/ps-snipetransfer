0b627ce04b8b335953483476d2e6d503f0d5fc95
dcbell <davidbell.personal@gmail.com>
2024-11-07 03:21:20 -0600

    converted to psm1 and updated connection logic

--- ps-snipetransfer.psm1
+++ ps-snipetransfer.psm1
@@ -1,8 +1,47 @@
-### Connection info. Set URL and API personal access token.
-$SnipeURL = Get-Content -Path './secure/url.txt' -Raw
-$ApiKey = Get-Content -Path './secure/key.txt' -Raw
+function Initialize-SnipeConnection {
+	[CmdletBinding()]
+	<#
+		.SYNOPSIS
+		Set connection variables for other modules.
+
+		.DESCRIPTION
+		Set connection variables for other modules.
+
+		.PARAMETER SnipeURL
+		[string] Required. URL of your Snipe-IT instance with no trailing slash. i.e. "https://org.snipe-it.io"
+
+		.PARAMETER ApiKey
+		[string] Required. API Personal Access Token for your Snipe-IT instance. For help go here https://snipe-it.readme.io/reference/generating-api-tokens
+	#>
+
+	param (
+		[Parameter(Mandatory=$true)]
+		[string]$SnipeURL,
+
+		[Parameter(Mandatory=$True)]
+		[string]$ApiKey
+	)
+
+	$script:SnipeURL = $SnipeURL
+	$script:ApiKey = $ApiKey
+	$script:ConnectionTested = $false
+	
+	# Validate connection
+	$response = $null
+	$headers=@{}
+	$headers.Add("accept", "application/json")
+	$headers.Add("Authorization", "Bearer $script:ApiKey")
+	$ApiUrl = "$script:SnipeURL/api/v1/user?limit=5" 
+	$response = Invoke-WebRequest -Uri $ApiUrl -Method GET -Headers $headers
+
+	if($response.Count -gt 0) { 
+		Write-Host "Connection verified." 
+		$script:ConnectionTested = $true
+	} else { Throw "Connection failure. Verify URL and API key and make sure at least 1 user exists in Snipe-IT." }
+}
 
 function New-SnipeCheckin {
+	[CmdletBinding()]
 	<#
 		.SYNOPSIS
 		Check in an asset or accessory.
@@ -25,19 +64,21 @@ function New-SnipeCheckin {
         [int]$CheckinID
 	)
 
+	if($script:ConnectionTested -eq $false) { Throw "Connection not established. Please run Initialize-SnipeConnection first." }
+
 	# Set API headers
 	$headers=@{}
 	$headers.Add("accept", "application/json")
-	$headers.Add("Authorization", "Bearer $ApiKey")
+	$headers.Add("Authorization", "Bearer $script:ApiKey")
 
 	switch ($CheckinType) {
 		"asset" { 
 			$headers.Add("content-type", "application/json")
-			$ApiUrl = "$SnipeURL/api/v1/hardware/$CheckinID/checkin" 
+			$ApiUrl = "$script:SnipeURL/api/v1/hardware/$CheckinID/checkin" 
 			$response = Invoke-WebRequest -Uri $ApiUrl -Method POST -Headers $headers -ContentType 'application/json' -Body "{ `"status_id`":null}" | ConvertFrom-Json
 		}
 		"accessory" { 
-			$ApiUrl = "$SnipeURL/api/v1/accessories/$CheckinID/checkin"
+			$ApiUrl = "$script:SnipeURL/api/v1/accessories/$CheckinID/checkin"
 			$response = Invoke-WebRequest -Uri $ApiUrl -Method POST -Headers $headers | ConvertFrom-Json
 		}
 		Default { Throw "Invalid object type. Value must be 'asset' or 'accessory'. Value provided was $CheckinType." }
@@ -47,6 +88,7 @@ function New-SnipeCheckin {
 }
 
 function New-SnipeCheckout {
+	[CmdletBinding()]
 	<#
 		.SYNOPSIS
 		Check out an asset or accessory.
@@ -81,6 +123,8 @@ function New-SnipeCheckout {
 		[int]$CheckoutToID
 	)
 
+	if($script:ConnectionTested -eq $false) { Throw "Connection not established. Please run Initialize-SnipeConnection first." }
+
 	# Check the CheckoutToType
 	if ($CheckoutType -eq "asset" -and $CheckoutToType -notin @("user", "location", "asset")) { throw "Invalid type to check out to. Value must be 'user', 'location', or 'asset'." }
 	if ($CheckoutType -eq "accessory" -and $CheckoutToType -notin @("user")) { throw "Invalid type to check out to. Accessories can only be checked out to 'user'. This is a Snipe-IT limitation." }
@@ -88,16 +132,16 @@ function New-SnipeCheckout {
 	# Set API headers
 	$headers=@{}
 	$headers.Add("accept", "application/json")
-	$headers.Add("Authorization", "Bearer $ApiKey")
+	$headers.Add("Authorization", "Bearer $script:ApiKey")
 	$headers.Add("content-type", "application/json")
 
 	switch ($CheckoutType) {
 		"asset" { 
-			$ApiUrl = "$SnipeURL/api/v1/hardware/$CheckoutID/checkout" 
+			$ApiUrl = "$script:SnipeURL/api/v1/hardware/$CheckoutID/checkout" 
 			$response = Invoke-WebRequest -Uri $ApiUrl -Method POST -Headers $headers -ContentType 'application/json' -Body "{ `"checkout_to_type`":`"$CheckoutToType`", `"assigned_$CheckoutToType`":$CheckoutToID }" | ConvertFrom-Json
 		}
 		"accessory" { 
-			$ApiUrl = "$SnipeURL/api/v1/accessories/$CheckoutID/checkout"
+			$ApiUrl = "$script:SnipeURL/api/v1/accessories/$CheckoutID/checkout"
 			$response = Invoke-WebRequest -Uri $ApiUrl -Method POST -Headers $headers -ContentType 'application/json' -Body "{ `"checkout_qty`":1,`"assigned_$CheckoutToType`":$CheckoutToID }" | ConvertFrom-Json
 		}
 		Default { Throw "Invalid object type. Value must be 'asset' or 'accessory'. Value provided was $CheckoutType." }
@@ -107,6 +151,7 @@ function New-SnipeCheckout {
 }
 
 function New-SnipeTransfer {
+	[CmdletBinding()]
 	<#
 		.SYNOPSIS
 		Transfer an asset or accessory from one user, location, or asset to another.
@@ -147,6 +192,8 @@ function New-SnipeTransfer {
 		[int]$AccessoryID
     )
 
+	if($script:ConnectionTested -eq $false) { Throw "Connection not established. Please run Initialize-SnipeConnection first." }
+
 	# Check in the object
 	Write-Host "Checking in $TransferType $TransferID"
 	New-SnipeCheckin -CheckinType $TransferType -CheckinID $TransferID
@@ -166,6 +213,7 @@ function New-SnipeTransfer {
 }
 
 function New-SnipeTransferAll {
+	[CmdletBinding()]
 	<#
 		.SYNOPSIS
 		Transfer all assets and accessories from one user to another.
@@ -188,6 +236,8 @@ function New-SnipeTransferAll {
         [int]$ToID
     )
 
+	if($script:ConnectionTested -eq $false) { Throw "Connection not established. Please run Initialize-SnipeConnection first." }
+
 	#Forcing FromType and ToType If they add pulling all checked out assets/accessories to locations and assets, this will become a parameter.
 	$FromType="user"
 	$ToType = "user"
@@ -195,7 +245,7 @@ function New-SnipeTransferAll {
 	# Set API headers
 	$headers=@{}
 	$headers.Add("accept", "application/json")
-	$headers.Add("Authorization", "Bearer $ApiKey")
+	$headers.Add("Authorization", "Bearer $script:ApiKey")
 
 	$AssetsToTransfer=@()
 	$AccessoryIDs=@()
@@ -203,9 +253,9 @@ function New-SnipeTransferAll {
 	switch ($FromType) {
 		"user" {
 			Write-Host "Fetching assets and accessories assigned to $FromType $FromID"
-			$ApiUrl = "$SnipeURL/api/v1/users/$FromID/assets"
+			$ApiUrl = "$script:SnipeURL/api/v1/users/$FromID/assets"
 			$AssetsToTransfer = (Invoke-WebRequest -Uri $ApiUrl -Method GET -Headers $headers  | ConvertFrom-Json).rows
-			$ApiUrl = "$SnipeURL/api/v1/users/$FromID/accessories"
+			$ApiUrl = "$script:SnipeURL/api/v1/users/$FromID/accessories"
 			$AccessoryIDs = (Invoke-WebRequest -Uri $ApiUrl -Method GET -Headers $headers  | ConvertFrom-Json).rows
 			$AccessoryIDs = $AccessoryIDs | Sort-Object id | Select-Object -Unique id
 		 }
@@ -217,7 +267,7 @@ function New-SnipeTransferAll {
 	$AccessoryIDs | foreach-object {
 		$TempAssetID = $_.id
 		# get a list of all checked out instances of this accessory
-		$ApiUrl = "$SnipeURL/api/v1/accessories/$($_.id)/checkedout"
+		$ApiUrl = "$script:SnipeURL/api/v1/accessories/$($_.id)/checkedout"
 		$response = (Invoke-WebRequest -Uri $ApiUrl -Method GET -Headers $headers  | ConvertFrom-Json).rows
 		# check each checked out instance to see if the user matches the FromID
 		$response | foreach-object {
